1920834d1acafe62f10bded1bc18dccd7e4da7cc
x303429874 <303429874@qq.com>
2014-07-12 23:35:03 +0800

    搜索

--- tuishudan.com/protected/controllers/SearchController.php
+++ tuishudan.com/protected/controllers/SearchController.php
@@ -7,29 +7,21 @@ class SearchController extends MyController
 {
 	public function actionIndex()
 	{
+		// 参数
 		$search_name = trim(Yii::app()->request->getParam('search'));
 
-		Books::model()->findAll("username=:name",array(":name"=>$username));    
-
-
-		$search_name = Yii::app()->request->getParam('search');
 		// 查询书
-		$books = Books::model()->findAll();
+		$cdb = new CDbCriteria();
+		if($search_name)
+		{
+			$cdb->addCondition("name like :name");
+			$cdb->params = array(":name" => "%{$search_name}%");
+		}
+		$rows = Books::model()->findAll($cdb);
 
-		$data = array('books' => $books);
-		$this->css = 'index';
+		// 显示
+		$data = array('rows' => $rows, 'search_name' => $search_name);
+		$this->css = 'mybook';
 		$this->render('index', $data);
-
-
-		$dataProvider=new CActiveDataProvider('Post', array(
-		    'criteria'=>array(
-		        'condition'=>'status=1',
-		        'order'=>'create_time DESC',
-		        'with'=>array('author'),
-		    ),
-		    'pagination'=>array(
-		        'pageSize'=>20,
-		    ),
-		));
 	}
 }
---
+++ tuishudan.com/protected/views/search/index.php
@@ -0,0 +1,20 @@
+<div class="fsb_title"><p><span class="f1">"<?php echo $search_name?>"相关的书</span></p></div>
+<div style="margin-top: 20px;"><img src="/images/line.png" /></div>
+<div id="main">
+<?php foreach($rows as $row):?>
+	<div class="list">
+	    <div class="pic">
+	        <a title="<?php echo $row->name?>" target="_blank" href="/book/<?php echo $row->id?>.html">
+	            <img src="<?php echo Yii::app()->params['cover_img_url'] . $row->cover_img?>" alt="<?php echo $row->name?>" height="150" width="120">
+	        </a>
+	    </div>
+	    <div class="content">
+	        <h3>
+	            <a title="<?php echo $row->name?>" target="_blank" class="top-title" href="/book/<?php echo $row->id?>.html"><?php echo $row->name?></a>
+	        </h3>
+	        <p>作者：<?php echo $row->author?></p>
+	        <p><span class="vote-star"><i style="width:50%"></i></span></p>
+	    </div>
+	</div>
+<?php endforeach;?>
+</div>
